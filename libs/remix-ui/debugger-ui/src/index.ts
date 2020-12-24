553fdcaacfc872c87f71061d42db94cb33680fd1
Iuri Matias <iuri.matias@gmail.com>
2020-12-24 11:45:48 -0500

    move settings environment selector to react

--- libs/remix-ui/debugger-ui/src/index.ts
+++ libs/remix-ui/debugger-ui/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/debugger-ui'
 export * from './lib/idebugger-api'
-export * from './lib/idebugger-api'
+// export * from './lib/environment-selector'
+export * from './lib/run-tab'
