2468178c568bd9d8a16bc13d5e8d0660611b3250
Iuri Matias <iuri.matias@gmail.com>
2021-01-31 11:18:20 -0500

    fix ident & linting issues

--- libs/remix-ui/run-tab/src/lib/settings/settings.tsx
+++ libs/remix-ui/run-tab/src/lib/settings/settings.tsx
@@ -7,16 +7,16 @@ import ValueSelector from './value-selector'
 import './settings.css'
 
 export const Settings = (props: any) => {
-    const {updateNetwork, newAccount, signMessage, copyToClipboard, options, personalModeChecked, selectedProvider, accounts} = props
-    return (
-        <div className="remixui_settings">
-          <EnvironmentSelector options={options} updateNetwork={updateNetwork} selectedProvider={selectedProvider} />
-          {/* ${networkEl} */}
-          <AccountSelector accounts={accounts} newAccount={newAccount} signMessage={signMessage} copyToClipboard={copyToClipboard} selectedProvider={selectedProvider} personalModeChecked={personalModeChecked} />
-          <GasPrice />
-          <ValueSelector />
-        </div>
-    )
+  const { updateNetwork, newAccount, signMessage, copyToClipboard, options, personalModeChecked, selectedProvider, accounts } = props
+  return (
+    <div className="remixui_settings">
+      <EnvironmentSelector options={options} updateNetwork={updateNetwork} selectedProvider={selectedProvider} />
+      {/* ${networkEl} */}
+      <AccountSelector accounts={accounts} newAccount={newAccount} signMessage={signMessage} copyToClipboard={copyToClipboard} selectedProvider={selectedProvider} personalModeChecked={personalModeChecked} />
+      <GasPrice />
+      <ValueSelector />
+    </div>
+  )
 }
 
 export default Settings
